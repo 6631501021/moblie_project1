81e7b6ec79c6cf8499e3711e4cc66279bc683e20
6631501021 <6631501021@lamduan.mfu.ac.th>
2025-08-26 16:22:11 +0700

    Login@foundation

--- moblie_projaect1/bin/moblie_projaect1.dart
+++ moblie_projaect1/bin/moblie_projaect1.dart
@@ -1,6 +1,69 @@
-import 'package:moblie_projaect1/moblie_projaect1.dart' as moblie_projaect1;
+// Install Package: dart pub add http
 
-void main(List<String> arguments) {
-  print('Hello world: ${moblie_projaect1.calculate()}!');
-  dddd
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
+
+class Expense {
+  int id;
+  String item;
+  double amount;
+  DateTime date;
+
+  Expense(this.id, this.item, this.amount, this.date);
+
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    return Expense(
+      json['id'],
+      json['item'],
+      json['paid'].toDouble(),
+      DateTime.parse(json['date']),
+    );
+  }
+}
+
+Future<void> main() async {
+  await login();
+}
+
+// Login ****Nook****
+Future<void> login() async {
+  print("===== Login =====");
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return;
+  }
+
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+
+  if (response.statusCode == 200) {
+    // Assuming response.body contains "Login OK" or user details
+    String loggedInUser = username; // Use username for now
+    await runExpenseApp(loggedInUser);
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print("Unknown error");
+  }
+}
+
+// ****Nook****
+Future<List<Expense>> fetchExpenses(String username) async {
+  final url = Uri.parse('http://localhost:3000/expenses/$username');
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    List<dynamic> data = jsonDecode(response.body);
+    return data.map((json) => Expense.fromJson(json)).toList();
+  } else {
+    print("Failed to load expenses: ${response.body}");
+    return [];
+  }
 }
