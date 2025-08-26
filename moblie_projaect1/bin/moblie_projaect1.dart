a77ca2d68b32d1bd5137741c436f2cedc46406bb
6631501021 <6631501021@lamduan.mfu.ac.th>
2025-08-26 15:58:19 +0700

    main

---
+++ moblie_projaect1/bin/moblie_projaect1.dart
@@ -0,0 +1,5 @@
+import 'package:moblie_projaect1/moblie_projaect1.dart' as moblie_projaect1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${moblie_projaect1.calculate()}!');
+}
---
+++ moblie_projaect1/test/moblie_projaect1_test.dart
@@ -0,0 +1,8 @@
+import 'package:moblie_projaect1/moblie_projaect1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
